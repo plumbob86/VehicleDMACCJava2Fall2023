d6d26a9c78b0623533070fbdd1b8310c63dafd39
plumbob86 <plumbob86@users.noreply.github.com>
2023-08-23 14:56:07 -0500

    Added VehicleNoise

---
+++ Vehicle/src/VehicleNoise.java
@@ -0,0 +1,15 @@
+import model.Example;
+
+public class VehicleNoise {
+	
+	public static void main(String[] args) {
+		Example example = new Example();
+		System.out.println(example.makeNoise());
+		
+		/*
+		 * Add a call to your vehicle below this comment.
+		 */
+
+	}
+
+}
